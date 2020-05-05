dadd70ec8acb892de6b95d060bc233b123386487
JJ MacFarlan <jj.macfarlan@gmail.com>
2020-05-04 22:26:20 -0600

    add TestFasterDictionary.cs

---
+++ Svelto.ECS.Tests/Common/DataStructures/TestFasterDictionary.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Svelto.DataStructures;
+using Assert = NUnit.Framework.Assert;
+
+namespace Svelto.ECS.Tests.Common.DataStructures
+{
+    [TestFixture]
+    class TestFasterDictionary
+    {
+        [TestCase]
+        public void TestUniqueKey()
+        {
+            FasterDictionary<int, string> test = new FasterDictionary<int, string>();
+
+            test.Add(1, "one.a");
+            void TestAddDup()
+            {
+                test.Add(1, "one.b");
+            }
+
+            Assert.Throws(typeof(FasterDictionaryException), TestAddDup);
+        }
+    }
+}
